0e6dc2929b8dc592e6a2ac7557f7e7b33acb3c38
yus2on <gbwl2784@gmail.com>
2020-12-15 00:08:09 +0900

    feat: Post Controller And Comment

--- src/main/java/com/rubypaper/domain/comment/Comment.java
+++ src/main/java/com/rubypaper/domain/comment/Comment.java
@@ -1,20 +1,28 @@
 package com.rubypaper.domain.comment;
 
 import com.rubypaper.domain.post.Post;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
+@Getter
+@Setter
 @Entity
 public class Comment {
-    @Id
-    @GeneratedValue
-    private Long id;
+    @Id @GeneratedValue
+    private Long id; // 코멘트 작성자 아이디
 
-    private String username;
+    private LocalDate regDate; // 코멘트 작성일
 
-    private String content;
+    private String username; // 코멘트 작성자 이름
+
+    private String content; // 코멘트 내용
 
     @ManyToOne
     @JoinColumn(name = "POST_ID")
     private Post post;
+
+
 }
--- src/main/java/com/rubypaper/domain/post/Post.java
+++ src/main/java/com/rubypaper/domain/post/Post.java
@@ -1,10 +1,14 @@
 package com.rubypaper.domain.post;
 
 import com.rubypaper.domain.category.Category;
+import com.rubypaper.domain.comment.Comment;
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,7 +22,25 @@ public class Post {
     @JoinColumn(name = "CATEGORY_ID")
     private Category category;
 
+    @NotNull
     private String title;
 
+    @NotNull
     private String content;
+
+    private LocalDateTime regDate;
+
+    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+    private List<Comment> comments;
+
+    public Post() { }
+
+    public Post(String title, String content, Category category) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+    }
+
+    public Post(Long id) {
+    }
 }
