69236d935d7fff120765434a5ff9be6913fc9123
Kimyechan <vlvkcjswo7@naver.com>
2020-12-15 11:38:26 +0900

    Fix: Reuqest Mapping Exception And Encoding Problem

--- src/main/java/com/rubypaper/controller/UserController.java
+++ src/main/java/com/rubypaper/controller/UserController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("/")
-    public String index(){
-        return "blogsystem_search";
-    }
+//    @GetMapping("/")
+//    public String index(){
+//        return "blogsystem_search";
+//    }
 
     @GetMapping("/login")
     public String login(){
--- src/main/java/com/rubypaper/repository/BlogRepository.java
+++ src/main/java/com/rubypaper/repository/BlogRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface BlogRepository extends JpaRepository<Blog, Long> {
     List<Blog> findByTitle(String searchKeyword);
 
-    @Query("select b from Blog b join fetch b.user where b.user.username = :searchKeyword")
+    @Query("select b from Blog b join fetch b.user where b.user.name = :searchKeyword")
     List<Blog> findByUsername(String searchKeyword);
 
     List<Blog> findByTag(String searchKeyword);
